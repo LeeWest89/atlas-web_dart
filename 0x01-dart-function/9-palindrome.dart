fccc0414d36e07a12702058e5250af4cab55d0c6
LeeWest89 <6683@holbertonstudents.com>
2024-10-04 14:02:36 -0500

    using a simple for loop to tell if a string is a palindrome or not

---
+++ 0x01-dart-function/9-palindrome.dart
@@ -0,0 +1,16 @@
+// Return if String is a Plaindrome or not
+bool isPalindrome(String s) {
+  int a = 0;
+  int b = s.length - 1;
+
+  if (s.length < 3) {
+    return (false);
+  }
+
+  for (; a < b; a++, b--) {
+    if (s[a] != s[b]) {
+      return (false);
+    }
+  }
+  return (true);
+}

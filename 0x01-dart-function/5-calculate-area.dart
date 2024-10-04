8386469d1e75b87bfbc1ef49b0bbb551bd8ffe90
LeeWest89 <6683@holbertonstudents.com>
2024-10-04 12:57:39 -0500

    Nice to be working with Geometry again, almost forgot I need to return a double

---
+++ 0x01-dart-function/5-calculate-area.dart
@@ -0,0 +1,5 @@
+// Fuction to return the area of a triangle
+double calculateArea(double height, double base) {
+  double triangleArea = (0.5 * base * height);
+  return (double.parse(triangleArea.toStringAsFixed(2)));
+}

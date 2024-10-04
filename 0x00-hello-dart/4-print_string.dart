2622919aed1a1022bb9c6b9bfcb096836560f728
LeeWest89 <6683@holbertonstudents.com>
2024-10-04 11:28:33 -0500

    had a 6th line

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,5 +1,4 @@
 // Function to print 3 times a string stored in the variable str, followed by its first 9 characters
-
 void main() {
   String str = "Holberton School";
   print('${str * 3}\n${str.substring(0, 9)}\n');

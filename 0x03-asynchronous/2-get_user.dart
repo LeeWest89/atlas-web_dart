5d502f970b8ee19ae18180121b1399f467a8c011
LeeWest89 <6683@holbertonstudents.com>
2024-10-07 17:02:11 -0500

    prints user as a string or prints the error

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,10 @@
+// Function that prints the user's string representation
+import '2-util.dart';
+
+Future<void> getUser() async {
+  try {
+    print(await fetchUser());
+  } catch (error) {
+    print('error caught: $error');
+  }
+}
